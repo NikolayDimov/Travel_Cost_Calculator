1e1ee5b39a6b6989709f8c0e4fc2e6add6d3b944
Nikolay Dimov <dimovv.nikolay@gmail.com>
2025-03-13 13:52:33 +0200

    feat(app) add html structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
-
+        <label htmlFor="starting_point">Starting Point</label>
+        <input id="starting_point" placeholder="Madrid" name="starting_point"></input>
+      </div>
+      <div>
+        <label htmlFor="destination_1">Destination</label>
+        <input id="destination_1" placeholder="London" name="destination_1"></input>
+      </div>
+      <button>+ Add Destination</button>
+      <select>
+        <option>Car</option>
+        <option>Train</option>
+        <option>Bus</option>
+      </select>
+      <div>
+        <label htmlFor="expense_name">Expense Name</label>
+        <input id="expense_name" placeholder="" name="expense_name"></input>
+      </div>
+      <div>
+        <p>Total Amount: $456</p>
+        <button type="submit">Submit</button>
       </div>
     </>
   )
